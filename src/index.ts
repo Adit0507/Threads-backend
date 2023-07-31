92b32d4d4948798463f3ad49f2216cd393d3cab5
aditya-singh-skilzen <aditya.kumar@skilzen.com>
2023-08-01 00:21:03 +0530

    apollo server setup

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,39 @@ import express from "express";
 import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from "@apollo/server/express4";
 
-const app = express();
-const PORT = Number(process.env.PORT) || 8000;
+async function init() {
+    const app = express();
+    const PORT = Number(process.env.PORT) || 8000;
+    
+    app.use(express.json())
 
-app.get("/", (req, res) => {
-  res.json({ message: "Server is running 🚀" });
-});
+    // creating graphql server
+    const gqlServer = new ApolloServer({
+        typeDefs: `
+            type Query {
+                hello: String
+                say(name: String): String
+            }
+        `,
+        resolvers: {
+            Query: {
+                hello: ()=> `Heyy there, I'm a GraphQL server 😊`,
+                say: (_, {name}: {name: String}) => `Hey ${name}, How are you`
+            }
+        }
+    })
+    
+    // Start gql server
+    await gqlServer.start()
+    
+    app.get("/", (req, res) => {
+      res.json({ message: "Server is running 🚀" });
+    });
 
-app.listen(PORT, () => console.log(`Served started at PORT: ${PORT} 🎉`));
+    app.use('/graphql', expressMiddleware(gqlServer))
+
+
+    app.listen(PORT, () => console.log(`Served started at PORT: ${PORT} 🎉`));
+}
+
+init()
