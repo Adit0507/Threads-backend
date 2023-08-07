3e7fd0d6524e653f06f582b2cf3d9d0dacbb35bd
aditya-singh-skilzen <aditya.kumar@skilzen.com>
2023-08-07 13:13:24 +0530

    minor change

--- src/graphql/user/mutations.ts
+++ src/graphql/user/mutations.ts
@@ -1,4 +1,4 @@
 // by createUser() we are returning an ID for the user
 export const mutations = `#graphql
-    createUser(firstName: String!, lastName: String!, email: String!, password: String!): String
+    createUser(firstName: String!, lastName: String, email: String!, password: String!): String
 `
